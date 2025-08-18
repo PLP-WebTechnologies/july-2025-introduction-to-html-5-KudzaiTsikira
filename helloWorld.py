4aa4f6d22d822f4a6ef58e5b9ea7477b05507d21
KudzaiTsikira <84228129+KudzaiTsikira@users.noreply.github.com>
2025-08-18 12:21:29 +0200

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+print("Hello world");
+def add():
+    num1 = int(input('enter a number \t'));
+    num2 = int(input('enter a second number \t'));
+    total = num1 + num2
+    print (total);
+
+add();
+
+
